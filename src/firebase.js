2e1b4f40295aa72b5b491267614b618101b0d640
keaton <bsemailme@gmail.com>
2020-11-25 15:58:06 -0700

    Change folders

---
+++ src/firebase.js
@@ -0,0 +1,35 @@
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+// firebase init - add your own config here
+const firebaseConfig = {
+  apiKey: "AIzaSyD0vOS5_-pCdY5IpCq-XqTFavqcnTs49C0",
+  authDomain: "hiddenvalleykings-a6ead.firebaseapp.com",
+  databaseURL: "https://hiddenvalleykings-a6ead.firebaseio.com",
+  projectId: "hiddenvalleykings-a6ead",
+  storageBucket: "hiddenvalleykings-a6ead.appspot.com",
+  messagingSenderId: "827809100083",
+  appId: "1:827809100083:web:6c0ec72d013ce99d078e46"
+}
+firebase.initializeApp(firebaseConfig)
+
+// utils
+const db = firebase.firestore()
+const auth = firebase.auth()
+
+// collection references
+const usersCollection = db.collection('users')
+const postsCollection = db.collection('posts')
+const commentsCollection = db.collection('comments')
+const likesCollection = db.collection('likes')
+
+// export utils/refs
+export {
+  db,
+  auth,
+  usersCollection,
+  postsCollection,
+  commentsCollection,
+  likesCollection
+}
---
+++ src/store/index.js
@@ -0,0 +1,44 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import * as fb from '../firebase'
+import router from '../router/index'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    fetchUserProfile: {}
+  },
+  mutations: {
+    setUserProfile (state, val) {
+      state.userProfile = val
+    }
+  },
+  actions: {
+    async login ({ dispatch }, form) {
+      // sign user in
+      const { user } = await fb.auth.signInWithEmailAndPassword(form.email, form.password)
+      // fetch user profile and set in state
+      dispatch('fetchUserProfile', user)
+    },
+    async fetchUserProfile ({ commit }, user) {
+      // fetch user profile
+      const userProfile = await fb.usersCollection.doc(user.uid).get()
+      // set user profile in store
+      commit('setUserProfile', userProfile.data())
+      // change route to dashboard
+      router.push('/')
+    },
+    async signup ({ dispatch }, form) {
+      // sign user up
+      const { user } = await fb.auth.createUserWithEmailAndPassword(form.email, form.password)
+      // create user profile object in userCollections
+      await fb.usersCollection.doc(user.uid).set({
+        name: form.name,
+        title: form.title
+      })
+      // fetch user profile and set in state
+      dispatch('fetchUserProfile', user)
+    }
+  }
+})
